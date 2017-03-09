df3b041271be62b2c2e5bfc6d91c9bb66b6937f5
张伟 <zhangwei19890518@gmail.com>
2017-03-09 20:29:37 +0800

    join & quit group action

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -43,6 +43,32 @@ class GroupsController < ApplicationController
     redirect_to groups_path
   end
 
+  def join
+    @group = Group.find(params[:id])
+
+    if !current_user.is_member_of?(@group)
+      current_user.join!(@group)
+      flash[:notice] = "加入本讨论版成功！"
+    else
+      flash[:warning] = "你已经是本讨论版成员了！"
+    end
+
+    redirect_to group_path(@group)
+  end
+
+  def quit
+    !@group = Group.find(params[:id])
+
+    if current_user.is_member_of?(@group)
+      current_user.quit!(@group)
+      flash[:alert] = "已退出本讨论版！"
+    else
+      flash[:warning] = "你不是本讨论版成员，怎么退出 XD"
+    end
+
+    redirect_to group_path(@group)
+  end
+
   private
   def find_group_and_check_permission
     @group = Group.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups do
+    member do
+      post :join
+      post :quit
+    end
     resources :posts
   end
   root 'groups#index'
