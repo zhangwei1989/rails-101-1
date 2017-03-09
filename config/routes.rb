3b92f07c02ef2abcd21856d148a04bdb2ca9cbf5
张伟 <zhangwei19890518@gmail.com>
2017-03-09 18:23:03 +0800

    implement post create function

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups do
-    resourse :posts
+    resources :posts
   end
   root 'groups#index'
 end
